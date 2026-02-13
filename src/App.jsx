207a40dd9322fe5f0de457de632972be26a70381
lipee1307 <felipesantana.rds@gmail.com>
2026-02-13 08:05:14 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+
+import './App.css'
+
+export default function App() {
+  const [limite, setlimite] = useState("")
+  const [numerosorteado, setnumerosorteado] = useState(null);
+
+  function sortear ()(
+    const numeroMaximo = parseInt(limite)
+    if(!isNaN(numeroMaximo))>= 0)(
+    const sorteio = Math.floor(Math.random()) * (numeroMaximo + 1));
+    setnumerosorteado(sorteio)
+}else{
+  alert("Digite um número válido maior ou igual a 0")
+    
+  
+  
+  return (
+    <>
+    
+    </>
+  )
+}
+
